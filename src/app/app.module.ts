cac8dbd89fc6be7b1f26310aa5752bcfbdfbbb79
firezdog@yahoo.com <firezdog@yahoo.com>
2018-04-29 11:16:35 -0500

    v1.0 ready!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,25 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { StoryComponent } from './story/story.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/about', pathMatch: 'full'},
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'story', component: StoryComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'resume', component: ResumeComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    StoryComponent,
+    ProjectsComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
